035d3d527b2b2b7ecf1a0b425ee8ef504735690e
lby132 <quddbscjswow@naver.com>
2022-08-30 22:52:20 +0900

    stream

---
+++ src/java8/chap04/HighCaloriesNames.java
@@ -0,0 +1,25 @@
+package java8.chap04;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java8.chap04.Dish.menu;
+
+public class HighCaloriesNames {
+
+    public static void main(String[] args) {
+        List<String> names = menu.stream()
+                .filter(dish -> {
+                    System.out.println("filtering " + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("mapping " + dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(toList());
+        System.out.println(names);
+    }
+}
